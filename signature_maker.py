5974e0ff8834d3e398a3d4e0b8fc183d942b41e7
mooncat-greenpy <mooncat.greenpy@gmail.com>
2020-11-25 22:18:43 +0900

    Add save function

--- signature_maker.py
+++ signature_maker.py
@@ -1,6 +1,7 @@
 import pefile
 import os
 import sys
+import json
 
 
 def make_sig_table(path, offset, size, sig_table):
@@ -13,16 +14,17 @@ def make_sig_table(path, offset, size, sig_table):
 
     for i in range(len(data)):
         if data[i] not in sig_table[i]:
-            sig_table[i][data[i]] = 1
+            sig_table[i][data[i]] = {"count": 1, "paths": [path]}
         else:
-            sig_table[i][data[i]] += 1
+            sig_table[i][data[i]]["count"] += 1
+            sig_table[i][data[i]]["paths"].append(path)
 
 
 def make_signature(sig_table):
     signature = ""
     for i in sig_table:
         if len(i) == 1:
-            signature += "%02x" % list(i.keys())[0]
+            signature += "%02x" % int(list(i.keys())[0])
         elif len(i) > 1:
             signature += "??"
         else:
@@ -47,10 +49,21 @@ def get_pe_entry_point_offset(path):
 def get_signature(path, get_offset_callback):
     size = 0x20
     sig_table = init_sig_table(size)
+
+    json_path = os.path.basename(path) + ".json"
+    if os.path.exists(json_path):
+        with open(json_path, "r") as f:
+            sig_table = json.load(f)
+        return make_signature(sig_table)
+
     for i in os.listdir(path):
         new_path = os.path.join(path, i)
         offset = get_offset_callback(new_path)
         make_sig_table(new_path, offset, size, sig_table)
+
+    with open(json_path, "w") as f:
+        json.dump(sig_table, f, indent=4)
+
     return make_signature(sig_table)
 
 
